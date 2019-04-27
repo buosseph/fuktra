3ae14c9e466b816f6d3f7159c9f0e545fc4af4ac
Brian Uosseph <brian.uosseph@gmail.com>
2019-04-27 00:18:11 -0400

    Update simple example

--- examples/simple/src/index.ts
+++ examples/simple/src/index.ts
@@ -13,19 +13,38 @@ const extensions: Extension[] = [
 		start: () => console.log('Configuring extension...'),
 		prepare: context => {
 			const request = context.get('request');
-			console.log(`HTTP ${request.httpVersion} ${request.method} ${request.url}`);
+			console.log(`HTTP ${request.httpVersion} ${request.method} ${request.url}\n${JSON.stringify(request.headers, undefined, '\t')}`);
 		},
 		before: () => console.log('Dispatching...'),
 		after: () => console.log('Sending response...')
 	}
 ];
 
+const index = () => 'Hello world';
+
+const context = ({ context: { request } }: any) => {
+	const response = `HTTP ${request.httpVersion} ${request.method} ${request.url}`;
+	console.log(response);
+	return response;
+};
+
+const args = ({ args }: any) => {
+	const response = `Path arguments: ${args}`;
+	console.log(response);
+	return response;
+};
+
+const parameters = ({ parameters }: any) => {
+	const response = `Parameters: ${JSON.stringify(parameters, undefined, '\t')}`;
+	console.log(response);
+	return response;
+};
+
 const root = {
-	[Symbols.dispatch]: 'root',
-	foo: {
-		[Symbols.dispatch]: 'foo',
-		bar: () => 'bar'
-	}
+	[Symbols.dispatch]: index,
+	context,
+	'arguments': args,
+	parameters
 };
 
 const app = new Application(root, { extensions });
